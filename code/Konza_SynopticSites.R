20bbf9282d242424cd64c9bc4cbfc34a305d2234
samzipper <samuelczipper@gmail.com>
2021-05-04 15:58:43 -0500

    Update synoptic maps

--- code/Konza_SynopticSites.R
+++ code/Konza_SynopticSites.R
@@ -1,4 +1,4 @@
-## Konza_PlaceSTICs.R
+## Konza_SynopticSites.R
 # Modified from script by Nate Jones: https://github.com/bamaecohydro/AIMS_watersheds/blob/main/R/3_STIC_Placement_Fun.R
 
 #Load relevant packages
@@ -155,6 +155,7 @@ st_crs(streams)<-st_crs(dem@crs)
 
 #Crop sheds to basin
 streams<-streams[sheds,]
+streams$L1 <- seq(1, dim(streams)[1])
 
 #Bring twi, fac, and slope into R env
 twi<-raster(file.path(scratch_dir,"twi.tif"))
@@ -176,9 +177,18 @@ buffer_dist <- 100  # [m] buffer distance between STICs - cannot be closer than 
 # load existing points
 current_stics <- st_read(file.path("data", "STIClocations_20210421.kml")) %>% 
   sf::st_transform(crs = p) %>% 
-  st_zm()
+  st_zm() %>% 
+  dplyr::mutate(Description = "STIC")
 
-n_stics_current <- dim(current_stics)[1]
+# key sites that we want to put stics
+key_sites <- read_csv(file.path("data", "KeySites.csv")) %>% 
+  sf::st_as_sf(coords = c("Long", "Lat"), crs = 4326) %>% 
+  sf::st_transform(crs = p) %>% 
+  dplyr::mutate(Description = "Weir")
+
+set_sites <- dplyr::bind_rows(current_stics, key_sites)
+
+n_stics_current <- dim(set_sites)[1]
 
 #Convert stream network to point w/ TWI and Aws
 pnts<-
@@ -190,23 +200,25 @@ pnts<-
   st_as_sf(
     coords  = c('X','Y'), 
     crs = st_crs(dem@crs)) %>% 
+  dplyr::left_join(st_drop_geometry(streams)[,c("L1", "FID")], by = "L1") %>% 
+  dplyr::rename(StreamReach = FID) %>% 
   mutate(
     twi = extract(twi, .), 
     con_area_ha = extract(fac, .)/10000,
     elevation_m = extract(dem, .),
     slope = extract(slope, .), 
     dist = extract(dist, .),
-    pid = seq(1, nrow(.))) # a unique identifier for each point
+    pid = seq(1, nrow(.)))  # a unique identifier for each point
 
 # find closest pnt to current STIC locations
-current_stics$closest_pid <- NA
-current_stics$closest_pid_dist <- NA
-for (i in 1:dim(current_stics)[1]){
-  i_dist <- as.numeric(st_distance(current_stics[i,], pnts))
+set_sites$closest_pid <- NA
+set_sites$closest_pid_dist <- NA
+for (i in 1:dim(set_sites)[1]){
+  i_dist <- as.numeric(st_distance(set_sites[i,], pnts))
   # pid is the unique identifier in pnts
   # figure out the closest pid to each current stic
-  current_stics$closest_pid[i] <- pnts$pid[which.min(i_dist)]
-  current_stics$closest_pid_dist[i] <- i_dist[which.min(i_dist)]
+  set_sites$closest_pid[i] <- pnts$pid[which.min(i_dist)]
+  set_sites$closest_pid_dist[i] <- i_dist[which.min(i_dist)]
   
   # drop all scale_ranks that are within buffer distance, except the closest one
   pid_drop <- pnts$pid[i_dist <= buffer_dist]
@@ -220,18 +232,18 @@ for (i in 1:dim(current_stics)[1]){
 }
 
 # remove the closest scale rank to each point from the drop list, but don't drop from pnts yet
-pid_drop_all <- pid_drop_all[!(pid_drop_all %in% current_stics$closest_pid)]
+pid_drop_all <- pid_drop_all[!(pid_drop_all %in% set_sites$closest_pid)]
 
 # add in data from pnts
-current_stics[, c("twi", "con_area_ha", "elevation_m", "slope", "dist")] <- 
-  sf::st_drop_geometry(pnts)[match(current_stics$closest_pid, pnts$pid), 
+set_sites[, c("twi", "con_area_ha", "elevation_m", "slope", "dist")] <- 
+  sf::st_drop_geometry(pnts)[match(set_sites$closest_pid, pnts$pid), 
                              c("twi", "con_area_ha", "elevation_m", "slope", "dist")]
 
 # eliminate pnts with con_area > the WQ sensor location (the streamline extends past it)
 # and calculate the variables you want to use to distribute stics
 pnts_all <-
   pnts %>% 
-  subset(con_area_ha <= max(current_stics$con_area_ha)) %>% 
+  subset(con_area_ha <= max(set_sites$con_area_ha)) %>% 
   mutate(scale_twi = scale(twi), 
          scale_dist = scale(1/dist), 
          scale_area = scale(1/con_area_ha), 
@@ -242,37 +254,80 @@ pnts_all <-
   mutate(rank_group = floor(seq(from = 1, to = n_stics+0.99999, length.out = dim(.)[1])))
 #table(pnts_trim$rank_group)  # check if groups area equal in size - each has 576 or 577 points
 
+twi_ecdf <- ecdf(pnts_all$twi)
+dist_ecdf <- ecdf(pnts_all$dist)
+area_ecdf <- ecdf(pnts_all$con_area_ha)
+
+# calculate quantiles
+pnts_all$quantile_twi <- twi_ecdf(pnts_all$twi)
+pnts_all$quantile_dist <- dist_ecdf(pnts_all$dist)
+pnts_all$quantile_area <- area_ecdf(pnts_all$con_area_ha)
+
+# split quantiles up into breaks
+n_breaks <- 5
+quantile_breaks <- seq(0, 1, length.out = n_breaks+1)
+pnts_all$quantile_group_twi <- cut(pnts_all$quantile_twi, breaks = quantile_breaks, include.lowest = T)
+pnts_all$quantile_group_dist <- cut(pnts_all$quantile_dist, breaks = quantile_breaks, include.lowest = T)
+pnts_all$quantile_group_area <- cut(pnts_all$quantile_area, breaks = quantile_breaks, include.lowest = T)
+
+# combined twi and area quantile
+pnts_all$quantile_group_select <- paste0(pnts_all$quantile_group_twi, "_", pnts_all$quantile_group_area)
+
 # first, eliminate the groups that already have a stic based on the current locations, 
 # and all points within the buffer distance of those
 pnts_trim <- 
   pnts_all %>% 
-  subset(!(rank_group %in% pnts_all$rank_group[pnts_all$pid %in% current_stics$closest_pid])) %>% 
+  subset(!(rank_group %in% pnts_all$rank_group[pnts_all$pid %in% set_sites$closest_pid])) %>% 
   subset(!(pid %in% pid_drop_all))
 
+# define stream reaches you only want max of 1 sample on
+StreamReach_limit <- c(117, 121)
+ggplot(streams, aes(color = (FID %in% StreamReach_limit))) + geom_sf()
+
+# find closest pnt to current STIC locations
 # place the remaining synoptic sites
-synoptic_pids <- current_stics$closest_pid
+synoptic_pids <- set_sites$closest_pid
 groups_left <- unique(pnts_trim$rank_group) # rank_groups that still need stics
 set.seed(1)
 for (i in 1:length(groups_left)){
   g <- groups_left[i] # get rank_group
   g_pid <- sample(pnts_trim$pid[pnts_trim$rank_group == g], 1)  # select a pid from this group
+  g_reach <- pnts_trim$StreamReach[pnts_trim$pid == g_pid]
   synoptic_pids <- c(synoptic_pids, g_pid) # add to overall list of stics
   i_dist <- as.numeric(st_distance(pnts_trim[pnts_trim$pid == g_pid, ], pnts_trim)) # get distance from this point to all other points
-  pnts_trim <- pnts_trim[i_dist >= buffer_dist, ] # drop points within buffer distance
+  drop_dist <- which(i_dist < buffer_dist)
+  
+  # check if it is one of the limited stream reaches
+  if (g_reach %in% StreamReach_limit){
+    drop_reach <- which(pnts_trim$StreamReach == g_reach)
+  } else {
+    drop_reach <- integer(0)
+  }
+  
+  i_pnts_drop <- unique(c(drop_dist, drop_reach))
+  pnts_trim <- pnts_trim[-i_pnts_drop, ] # drop points that are within buffer distance or on same stream reach (if one of limited reach)
 }
 
 # add a column to the pnts_all data frame
 pnts_synoptic <- subset(pnts_all, pid %in% synoptic_pids) %>% 
   mutate(Site = "New")
-pnts_synoptic$Site[pnts_synoptic$pid %in% current_stics$closest_pid] <- "Current"
+pnts_synoptic$Site[pnts_synoptic$pid %in% set_sites$closest_pid[set_sites$Description == "STIC"]] <- "STIC"
+pnts_synoptic$Site[pnts_synoptic$pid %in% set_sites$closest_pid[set_sites$Description == "Weir"]] <- "Weir"
 
-#Plot for testing 
+## plot
+# load springs/seeps to put on map
+sf_springs <- read_csv(file.path("data", "SFKC_SeepsSprings.csv")) %>% 
+  sf::st_as_sf(coords = c("LONGdegW", "LATdegN"), crs = 4326) %>% 
+  sf::st_transform(crs = p) 
+
 p_map <-
   ggplot() +
   geom_sf(data = sheds, color = col.gray) +
   geom_sf(data = streams, color = "black") +
+  geom_sf(data = sf_springs, aes(shape = factor(SpringSeepClass))) +
   geom_sf(data = pnts_synoptic, aes(color = Site)) +
-  scale_color_manual(values = c("Current" = col.cat.red, "New" = col.cat.blu))
+  scale_color_manual(values = c("STIC" = col.cat.red, "Weir" = col.cat.org, "New" = col.cat.blu)) +
+  scale_shape_discrete(name = "Spring Class")
 
 # plot distribution of TWI and drainage area of selected points vs all points
 p_dist <-
@@ -281,12 +336,12 @@ p_dist <-
   geom_point(data = pnts_synoptic, aes(x = con_area_ha, y = twi, color = Site)) +
   scale_x_continuous(name = "Drainage Area [ha]") +
   scale_y_continuous(name = "TWI") +
-  scale_color_manual(values = c("Current" = col.cat.red, "New" = col.cat.blu))
+  scale_color_manual(values = c("STIC" = col.cat.red, "Weir" = col.cat.org, "New" = col.cat.blu))
 
 (p_map + p_dist) +
   plot_layout(ncol = 2, guides = "collect") + 
   plot_annotation(title = "Distributed based on TWI and drainage area",
-                  subtitle = "50 locations; red = existing STICs, blue = potential new sites") +
+                  subtitle = "50 locations; red = existing STICs, orange = weirs, blue = potential new sites") +
   ggsave(file.path("plots", "Konza_PlaceSTICs_Map+Dist.png"),
          width = 10, height = 4, units = "in")
 
@@ -321,6 +376,7 @@ m<-
   mapview(sheds,
           alpha.regions=0.3) +
   mapview(streams) +
+  mapview(sf_springs, zcol = 'SpringSeepClass', col.regions = c(col.cat.red, col.cat.org, col.cat.yel), color = "white") +
   mapview(pnts_synoptic, zcol='twi')
 m
 
