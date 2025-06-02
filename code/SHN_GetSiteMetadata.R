971121030d74e689b117230f4fb550f78c7cb6d2
Sam Zipper <samzipper@ku.edu>
2025-06-02 14:23:39 -0500

    Add slope extraction to YMR and SHN metadata

--- code/SHN_GetSiteMetadata.R
+++ code/SHN_GetSiteMetadata.R
@@ -18,6 +18,7 @@ sf_sites <- st_as_sf(df_sites, coords = c("long", "lat"), crs = st_crs(4326))
 r_dem <- rast(file.path("results", "konzaShane_DEM_m.tif"))
 r_twi <- rast(file.path("results", "konzaShane_TWI.tif"))
 r_flowacc <- rast(file.path("results", "konzaShane_FlowAccumulation_cells.tif"))
+r_slope <- rast(file.path("results", "konzaShane_Slope_degrees.tif"))
 
 # since lat/long of some poitns aren't exactly on mapped stream network - use 10m buffer
 sf_sites_buffer <- st_buffer(sf_sites, 10)
@@ -25,6 +26,7 @@ sf_sites_buffer <- st_buffer(sf_sites, 10)
 df_sites$elevation_m <- extract(r_dem, sf_sites_buffer, fun = min)[,2]
 df_sites$TWI <- extract(r_twi, sf_sites_buffer, fun = max)[,2]
 df_sites$drainageArea_ha <- extract(r_flowacc, sf_sites_buffer, fun = max)[,2]*res(r_flowacc)[1]*res(r_flowacc)[2]/10000
+df_sites$slope_degrees <- extract(r_slope, sf_sites_buffer, fun = mean)[,2]
 
 ggplot() +
   geom_spatraster(data = r_flowacc) +
